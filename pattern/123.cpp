e0183756dd2cdb3796c0f7ddb2a32f37b78174f3
Nitya-Gopal-Bhadra <nityarocks62@gmail.com>
2025-09-10 01:33:02 +0530

    Add number triangle pattern 123.cpp

---
+++ pattern/123.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // Your code starts here
+
+    for (int i = 0; i < 6; i++)
+    {
+        /* code */
+        for (int j = 0; j < i; j++)
+        {
+            /* code */
+            cout<< j ;
+        }
+        cout << endl;
+    }
+    
+    
+    return 0;
+}
